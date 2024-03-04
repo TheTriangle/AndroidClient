ec133b2d4cf7f6a0688df1e5068a16843c017c04
fukenrice <63079551+fukenrice@users.noreply.github.com>
2024-03-04 19:50:38 +0300

    Add patient code screen

---
+++ app/src/main/java/com/teamproject/wounddetection/viewmodel/AppViewModelProvider.kt
@@ -0,0 +1,28 @@
+package com.teamproject.wounddetection.viewmodel
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.teamproject.wounddetection.App
+import com.teamproject.wounddetection.data.api.PatientApi
+import com.teamproject.wounddetection.data.api.UserApi
+
+object AppViewModelProvider {
+    val Factory = viewModelFactory {
+        initializer {
+            AuthViewModel(
+                app().retrofit.create(UserApi::class.java)
+            )
+        }
+
+        initializer {
+            PatientDataViewModel(
+                app().retrofit.create(PatientApi::class.java)
+            )
+        }
+    }
+}
+
+fun CreationExtras.app(): App =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as App)
--- app/src/main/java/com/teamproject/wounddetection/viewmodel/AuthViewModel.kt
+++ app/src/main/java/com/teamproject/wounddetection/viewmodel/AuthViewModel.kt
@@ -1,45 +1,45 @@
 package com.teamproject.wounddetection.viewmodel
 
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
 import androidx.lifecycle.viewModelScope
-import androidx.lifecycle.viewmodel.CreationExtras
-import com.teamproject.wounddetection.App
 import com.teamproject.wounddetection.data.api.UserApi
 import com.teamproject.wounddetection.data.model.User
+import com.teamproject.wounddetection.utils.Resource
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import retrofit2.HttpException
 
-const val TAG = "AuthViewModel"
+
 
 class AuthViewModel(
     private val api: UserApi,
     private val dispatcher: CoroutineDispatcher = Dispatchers.IO
 ) : ViewModel() {
+
     companion object  {
-        val Factory: ViewModelProvider.Factory = object : ViewModelProvider.Factory {
-            override fun <T : ViewModel> create(modelClass: Class<T>, extras: CreationExtras): T {
-                val application = checkNotNull(extras[APPLICATION_KEY])
-                return AuthViewModel(
-                    (application as App).retrofit.create(UserApi::class.java)
-                ) as T
-            }
-        }
+        private const val TAG = "AuthViewModel"
     }
+
+    private val _auth = MutableLiveData<Resource<String>>()
+    val auth: LiveData<Resource<String>> = _auth
 
     fun makeSignInRequest(login: String, password: String) {
         viewModelScope.launch(dispatcher) {
             try {
+                _auth.postValue(Resource.loading(null))
                 api.login(User(login, password))
+                _auth.postValue(Resource.success(null))
             } catch (e: Throwable) {
                 if (e is HttpException) {
                     Log.d(TAG, "makeSignInRequest: " + (e.response()?.body() ?: e.response()))
+                    _auth.postValue(Resource.error(e.message(), null))
                 } else {
                     Log.d(TAG, "makeSignInRequest: " + e.message)
+                    _auth.postValue(Resource.error("An error occurred while logging in", null))
                 }
             }
         }
