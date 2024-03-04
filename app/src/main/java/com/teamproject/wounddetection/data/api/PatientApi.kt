033afbcfb593ffc48d1ab7c6ad116012b0132630
fukenrice <63079551+fukenrice@users.noreply.github.com>
2024-03-04 20:01:40 +0300

    Fix api method

--- app/src/main/java/com/teamproject/wounddetection/data/api/PatientApi.kt
+++ app/src/main/java/com/teamproject/wounddetection/data/api/PatientApi.kt
@@ -5,6 +5,6 @@ import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface PatientApi {
-    @GET("/verifyCode/{code}")
+    @GET("/getPatient/{code}")
     suspend fun getPatient(@Path("code") code: String): Patient
 }
